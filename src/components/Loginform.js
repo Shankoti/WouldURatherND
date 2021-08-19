cf8ef3d3db673686986d50ad8fa73a48f67515e1
Shankoti <mohamed.yasser2727@gmail.com>
2021-08-19 14:03:59 +0200

    firstSubmion

---
+++ src/components/Loginform.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { SetAuthedUser } from "./actions/authedUser";
+import { List, Header, Segment } from "semantic-ui-react";
+
+class Loginform extends Component {
+  state = {};
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Loginform;
