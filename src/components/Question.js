000db7fb509878952bbe1d555923317ca864f235
Shankoti <mohamed.yasser2727@gmail.com>
2021-08-20 13:32:50 +0200

    4.0

--- src/components/Question.js
+++ src/components/Question.js
@@ -19,121 +19,134 @@ class Question extends Component {
 
   render() {
     if (this.props.athusr.id) {
-      console.log(this.props);
-      const { Id, QUS, urs } = this.props;
-      console.log(QUS[Id]);
-      console.log(urs[QUS[Id].author].avatarURL);
-      const theQuestion = QUS[Id];
-      const author = urs[QUS[Id].author];
-      const answerd = Object.keys(this.props.urs[this.props.athusr.id].answers);
+      if (this.props.Id !== "undefined") {
+        const { Id, QUS, urs } = this.props;
+        const theQuestion = QUS[Id];
+        const author = urs[QUS[Id].author];
+        const answerd = Object.keys(
+          this.props.urs[this.props.athusr.id].answers
+        );
 
-      return (
-        <div>
-          <Nav />
-          <Container>
-            <Segment raised>
-              <Grid divided="vertically">
-                <Grid.Row columns={2}>
-                  <Grid.Column width="5">
-                    <Image src={author.avatarURL} width="170" height="170" />
-                  </Grid.Column>
-                  <Grid.Column textAlign="left" verticalAlign="middle">
-                    <Grid.Row columns={1}>
-                      <Grid.Column>
-                        <h1>{`Asked by ${author.name} `}</h1>
-                      </Grid.Column>
+        return (
+          <div>
+            <Nav />
+            <Container>
+              <Segment raised>
+                <Grid divided="vertically">
+                  <Grid.Row columns={2}>
+                    <Grid.Column width="5">
+                      <Image src={author.avatarURL} width="170" height="170" />
+                    </Grid.Column>
+                    <Grid.Column textAlign="left" verticalAlign="middle">
+                      <Grid.Row columns={1}>
+                        <Grid.Column>
+                          <h1>{`Asked by ${author.name} `}</h1>
+                        </Grid.Column>
 
-                      <Grid.Column>
-                        {this.state.vis || answerd.includes(theQuestion.id) ? (
-                          <h1>
-                            {`${theQuestion.optionOne.votes.length} of ${
-                              theQuestion.optionOne.votes.length +
-                              theQuestion.optionTwo.votes.length
-                            } Picked to `}
-                            {theQuestion.optionOne.text}
+                        <Grid.Column>
+                          {this.state.vis ||
+                          answerd.includes(theQuestion.id) ? (
+                            <h1>
+                              {`${theQuestion.optionOne.votes.length} of ${
+                                theQuestion.optionOne.votes.length +
+                                theQuestion.optionTwo.votes.length
+                              } Picked to `}
+                              {theQuestion.optionOne.text}
 
-                            <Progress
-                              percent={
-                                (theQuestion.optionOne.votes.length /
-                                  (theQuestion.optionOne.votes.length +
-                                    theQuestion.optionTwo.votes.length)) *
-                                100
-                              }
-                              progress
-                            />
-                          </h1>
-                        ) : (
-                          <Button
-                            onClick={() => {
-                              this.setState({ vis: true });
+                              <Progress
+                                percent={
+                                  (theQuestion.optionOne.votes.length /
+                                    (theQuestion.optionOne.votes.length +
+                                      theQuestion.optionTwo.votes.length)) *
+                                  100
+                                }
+                                progress
+                              />
+                            </h1>
+                          ) : (
+                            <Button
+                              onClick={() => {
+                                this.setState({ vis: true });
 
-                              this.props.dispatch(
-                                handleQanswer(
-                                  this.props.athusr.id,
-                                  theQuestion.id,
-                                  "optionOne"
-                                )
-                              );
-                            }}
-                            primary
-                          >{`${theQuestion.optionOne.text} `}</Button>
-                        )}
-                        {this.state.vis || answerd.includes(theQuestion.id) ? (
-                          <h1>
-                            {`${theQuestion.optionTwo.votes.length} of ${
-                              theQuestion.optionOne.votes.length +
-                              theQuestion.optionTwo.votes.length
-                            } Picked `}
-                            {theQuestion.optionTwo.text}
+                                this.props.dispatch(
+                                  handleQanswer(
+                                    this.props.athusr.id,
+                                    theQuestion.id,
+                                    "optionOne"
+                                  )
+                                );
+                              }}
+                              primary
+                            >{`${theQuestion.optionOne.text} `}</Button>
+                          )}
+                          {this.state.vis ||
+                          answerd.includes(theQuestion.id) ? (
+                            <h1>
+                              {`${theQuestion.optionTwo.votes.length} of ${
+                                theQuestion.optionOne.votes.length +
+                                theQuestion.optionTwo.votes.length
+                              } Picked `}
+                              {theQuestion.optionTwo.text}
 
-                            <Progress
-                              percent={
-                                (theQuestion.optionTwo.votes.length /
-                                  (theQuestion.optionOne.votes.length +
-                                    theQuestion.optionTwo.votes.length)) *
-                                100
-                              }
-                              progress
-                            />
-                            {this.props.urs[this.props.athusr.id].answers[
-                              theQuestion.id
-                            ] === "optionTwo" ? (
-                              <Header style={{ padding: 20 }}>
-                                Your answer is
-                                <Header color="green">{`${theQuestion.optionTwo.text}`}</Header>
-                              </Header>
-                            ) : (
-                              <Header style={{ padding: 20 }}>
-                                Your answer is
-                                <Header color="green">{`${theQuestion.optionOne.text}`}</Header>
-                              </Header>
-                            )}
-                          </h1>
-                        ) : (
-                          <Button
-                            onClick={() => {
-                              this.setState({ vis: true });
-                              this.props.dispatch(
-                                handleQanswer(
-                                  this.props.athusr.id,
-                                  theQuestion.id,
-                                  "optionTwo"
-                                )
-                              );
-                            }}
-                          >
-                            {`${theQuestion.optionTwo.text} `}
-                          </Button>
-                        )}
-                      </Grid.Column>
-                    </Grid.Row>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
+                              <Progress
+                                percent={
+                                  (theQuestion.optionTwo.votes.length /
+                                    (theQuestion.optionOne.votes.length +
+                                      theQuestion.optionTwo.votes.length)) *
+                                  100
+                                }
+                                progress
+                              />
+                              {this.props.urs[this.props.athusr.id].answers[
+                                theQuestion.id
+                              ] === "optionTwo" ? (
+                                <Header style={{ padding: 20 }}>
+                                  Your answer is
+                                  <Header color="green">{`${theQuestion.optionTwo.text}`}</Header>
+                                </Header>
+                              ) : (
+                                <Header style={{ padding: 20 }}>
+                                  Your answer is
+                                  <Header color="green">{`${theQuestion.optionOne.text}`}</Header>
+                                </Header>
+                              )}
+                            </h1>
+                          ) : (
+                            <Button
+                              onClick={() => {
+                                this.setState({ vis: true });
+                                this.props.dispatch(
+                                  handleQanswer(
+                                    this.props.athusr.id,
+                                    theQuestion.id,
+                                    "optionTwo"
+                                  )
+                                );
+                              }}
+                            >
+                              {`${theQuestion.optionTwo.text} `}
+                            </Button>
+                          )}
+                        </Grid.Column>
+                      </Grid.Row>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Segment>
+            </Container>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <Nav />
+            <Segment placeholder textAlign="center">
+              <Header size="large">404</Header>
+              <Header size="large">PAGE NOT FOUND</Header>
             </Segment>
-          </Container>
-        </div>
-      );
+          </div>
+        );
+      }
     } else {
       return (
         <Redirect
